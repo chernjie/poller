f4447bea2869777b83bcfb1f50ec59902e5115e8
CJ <lim@chernjie.com>
2022-03-15 13:18:07 -0700

    run once an hour only

--- jobs/gold-silver.sh
+++ jobs/gold-silver.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+test 17 = $(date +%M) || exit 1
+
 curl --silent -A Chrome https://www.uob.com.sg/data-api-rates/data-api/gold-silver/ |
 	jq '{ currentDate, time, types : (.types | map(select(.description=="GSA"))) }' |
 	sponge ./watch/gold-silver.json
