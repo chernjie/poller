0513dcbb249db6059f17ef16b366c89e61bd6a4d
CJ <lim@chernjie.com>
2022-03-15 01:20:03 -0700

    ./jobs/gold-silver.sh

--- jobs/gold-silver.sh
+++ jobs/gold-silver.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-curl --silent -A Chrome https://www.uob.com.sg/data-api-rates/data-api/gold-silver/ | jq . | sponge ./watch/gold-silver.json
+curl --silent -A Chrome https://www.uob.com.sg/data-api-rates/data-api/gold-silver/ |
+	jq '{ currentDate, time, types : (.types | map(select(.description=="GSA"))) }' |
+	sponge ./watch/gold-silver.json
