d297bef158c4b65db5aaff45a457ed8ab2f13da9
CJ <lim@chernjie.com>
2022-03-19 13:00:09 -0700

    use yaml

--- jobs/gold-silver.sh
+++ jobs/gold-silver.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-test 17 = $(date +%M) || exit 1
+test "03" = "$(date +%M)" || exit 1
 
-curl --silent -A Chrome https://www.uob.com.sg/data-api-rates/data-api/gold-silver/ |
+curl --silent -A Chrome https://www.uob.com.sg/data-api-rates/data-api/gold-silver/ \
+    --output ./data/gold-silver.json
+
+wc -c ./data/gold-silver.json | while read byte file
+do test 0 -eq $byte && exit 1
+done
+
+cat ./data/gold-silver.json |
 	jq '{ currentDate, time, types : (.types | map(select(.description=="GSA"))) }' |
-	sponge ./watch/gold-silver.json
+	npx js-yaml |
+	sponge ./watch/gold-silver.yaml
